db1476c70be46d53d96d91599b65e75d6315e51d
ajaysharma12799 <ajaysharma12799@gmail.com>
2020-07-23 20:05:26 +0530

    implemented flash-message system

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
+require('dotenv').config();
 const path = require('path');
 const express = require('express');
+const flash = require('connect-flash');
+const bodyParser = require('body-parser');
+const session = require('express-session');
 
 const indexRouter = require('./routes/index');
 
@@ -7,6 +11,22 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 app.use(express.static(path.join(__dirname, 'views')));
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use(session({ // EXPRESS-SESSION
+    secret: 'email_using_nodejs',
+    resave: true,
+    saveUninitialized: true
+}))
+
+app.use(flash()); // CONNECT-FLASH
+
+// GLOBAL VARS
+app.use( (req, res, next) => {
+    res.locals.success_msg = req.flash('success_msg');
+    res.locals.error_msg = req.flash('error_msg');
+    next();
+} )
 
 // Routes
 app.use(indexRouter);
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,31 @@
 const router = require('express').Router();
+const nodeMailer = require('nodemailer');
 
 router.get('/', (req, res) => {
     res.render('index.ejs');
+});
+
+router.post('/', (req, res) => {
+
+    let errors = [];
+    const { name, email, message } = req.body;
+
+    if( !name || !email || !message ) { // Checking Validation for all fields
+        errors.push({
+            message: 'Please Fill all Fields'
+        })
+    }
+
+    if(errors.length > 0) { // CHECKING WHETHER ERRORS-ARRAY CONTAIN ANY ERROR, IF YES THEN WE RENDER PAGE AGAIN WITH ERRORS
+        res.render('index');
+    }
+    else { // PASSING ALL VALIDATION FIELDS
+        // Todo: Work on NodeMailer here
+        console.log(req.body);
+        req.flash('success_msg', 'You Can now proceed');
+        res.redirect('/');
+    }
+
 });
 
 module.exports = router;
